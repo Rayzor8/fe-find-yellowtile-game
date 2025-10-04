6ef558d0ab53aeb2bdee540013c26620f7576d95
Ray <rayhidayat220@gmail.com>
2025-10-04 14:49:47 +0700

    feat: create blue tile keyboard handler

--- src/components/board-game/controls.tsx
+++ src/components/board-game/controls.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { ChevronUp, ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
+import React, { useCallback, useEffect } from "react";
+import {
+  ChevronUp,
+  ChevronDown,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 import { GridType } from "@/types";
 
 type ControlsProps = {
@@ -53,45 +58,76 @@ export default function Controls({
     }
   }
 
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent): void => {
+
+      if (gameOver) return;
+
+      if (event.key === "ArrowUp") {
+        event.preventDefault();
+        moveBlueTile("up");
+      } else if (event.key === "ArrowDown") {
+        event.preventDefault();
+        moveBlueTile("down");
+      } else if (event.key === "ArrowLeft") {
+        event.preventDefault();
+        moveBlueTile("left");
+      } else if (event.key === "ArrowRight") {
+        event.preventDefault();
+        moveBlueTile("right");
+      }
+    },
+    
+    [[gameOver, blueTile]]
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [gameOver, blueTile]);
+
   return (
     <div className="flex-1 xl:flex-none flex flex-col justify-center">
-    <div className="grid grid-cols-3 grid-rows-3 gap-1">
-      <button
-        onClick={() => moveBlueTile("up")}
-        className="bg-gray-500 px-auto hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 col-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
-        disabled={gameOver}
-        aria-label="up"
-      >
-        <ChevronUp size={24}  />
-      </button>
+      <div className="grid grid-cols-3 grid-rows-3 gap-1">
+        <button
+          onClick={() => moveBlueTile("up")}
+          className="bg-gray-500 px-auto hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 col-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
+          disabled={gameOver}
+          aria-label="up"
+        >
+          <ChevronUp size={24} />
+        </button>
 
-      <button
-        onClick={() => moveBlueTile("left")}
-        className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
-        disabled={gameOver}
-        aria-label="left"
-      >
-        <ChevronLeft size={24} />
-      </button>
+        <button
+          onClick={() => moveBlueTile("left")}
+          className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
+          disabled={gameOver}
+          aria-label="left"
+        >
+          <ChevronLeft size={24} />
+        </button>
 
-      <button
-        onClick={() => moveBlueTile("right")}
-        className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-2 col-start-3 cursor-pointer w-18 h-14 flex justify-center items-center"
-        disabled={gameOver}
-        aria-label="right"
-      >
-        <ChevronRight size={24} />
-      </button>
+        <button
+          onClick={() => moveBlueTile("right")}
+          className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-2 col-start-3 cursor-pointer w-18 h-14 flex justify-center items-center"
+          disabled={gameOver}
+          aria-label="right"
+        >
+          <ChevronRight size={24} />
+        </button>
 
-      <button
-        onClick={() => moveBlueTile("down")}
-        className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-3 col-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
-        disabled={gameOver}
-        aria-label="down"
-      >
-        <ChevronDown size={24} />
-      </button>  
-    </div>
+        <button
+          onClick={() => moveBlueTile("down")}
+          className="bg-gray-500 hover:bg-gray-700 text-white p-3 rounded-lg transition-colors disabled:opacity-50 row-start-3 col-start-2 cursor-pointer w-18 h-14 flex justify-center items-center"
+          disabled={gameOver}
+          aria-label="down"
+        >
+          <ChevronDown size={24} />
+        </button>
+      </div>
     </div>
   );
 }
